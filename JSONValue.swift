f4f719db1b40ccbfa0f1f8d030abd0d1c178d3fd
OutOfCode <34772312+outofcoding@users.noreply.github.com>
2018-12-25 10:48:52 +0900

    Create JSONValue.swift

---
+++ JSONValue.swift
@@ -0,0 +1,99 @@
+struct JSONValue<T: Decodable> : Decodable {
+    var value: T?
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let value = (try? container.decode(T.self)) {
+            self.value = value
+        }
+    }
+}
+
+struct JSONString : Decodable {
+    var value: String
+
+    init(_ value: String) {
+        self.value = value
+    }
+    
+    init(from decoder: Decoder) throws {
+        let value = try? DecodeValue(from: decoder)
+        switch value {
+        case .some(.string(let string)):
+            self.value = string
+        case .some(.int(let int)):
+            self.value = String(int)
+        case .some(.double(let double)):
+            self.value = String(double)
+        default:
+            self.value = ""
+        }
+    }
+}
+
+struct JSONInt : Decodable {
+    var value: Int
+    
+    init(_ value: Int) {
+        self.value = value
+    }
+    
+    init(from decoder: Decoder) throws {
+        let value = try? JSONValue(from: decoder)
+        switch value {
+        case .some(.int(let int)):
+            self.value = int
+        case .some(.string(let string)):
+            self.value = Int(string) ?? 0
+        case .some(.double(let double)):
+            self.value = Int(double)
+        case .some(.bool(let bool)):
+            self.value = bool ? 1 : 0
+        default:
+            self = 0
+        }
+    }
+}
+
+struct JSONDictionary<T: Decodable> : Decodable {
+    var value: [String: T]?
+    var list: T?
+    
+    var isNil: Bool {
+        return value == nil && list == nil
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let value = (try? container.decode([String: T].self)) {
+            self.value = value
+        } else if let list = (try? container.decode(T.self)) {
+            self.list = list
+        }
+    }
+}
+
+struct JSONArray<T: Decodable> : Decodable {
+    var value: [T]?
+    var list: T?
+    
+    var isNil: Bool {
+        return value == nil && list == nil
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let value = (try? container.decode([T].self)) {
+            self.value = value
+        } else if let list = (try? container.decode(T.self)) {
+            self.list = list
+        }
+    }
+}
